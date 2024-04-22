fce223a4174634f709a99d365ea44d51dea100ee
ChrisPytel <chris_pytel@hotmail.com>
2024-04-21 21:00:33 -0400

    move migrations, seeds, README and queries outside of webApp directory

--- LightBnB_WebApp/01_queries/02_avg_length_of_reservation.sql
+++
@@ -1,8 +0,0 @@
-/* Average Length Of Reservation
-
-Our product managers want a query to see the average duration of a reservation.
-
-=> Get the average duration of all reservations. */
-
-SELECT AVG(end_date - start_date) AS average_reservation_length
-FROM reservations;
